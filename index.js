30cff890c21637d66f1a3a2b5568b4472dbc130c
codewithshamim <shamimislamonline@gmail.com>
2022-07-24 15:04:26 +0600

    send data server to cllent

--- index.js
+++ index.js
@@ -4,8 +4,35 @@ const app = express();
 const http = require("http");
 const expressServer = http.createServer(app);
 
+// create socket server 
+const { Server } = require("socket.io");
+const io = new Server(expressServer);
+
+
+
+let user = 0;
+io.on("connection", (socket) => {
+    user++;
+    console.log("new user connected", user);
+    socket.on("disconnect", () => {
+        user--;
+        console.log("user disconnected", user);
+    })
+
+    // send data to client
+    socket.send(user);
+
+
+    setInterval(() => {
+        const date = new Date();
+        const getDate = date.getTime();
+        socket.emit("myTimeEvent", getDate)
+    }, 1000)
+})
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html")
+        // res.send(`<h1>socket.io server...</h1>`)
 })
 expressServer.listen(port, () => {
     console.log("Listening to port", port)
